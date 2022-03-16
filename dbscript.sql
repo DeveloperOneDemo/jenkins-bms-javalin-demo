be2645a8f11e89ba3f8112a3ab43fbae41801340
DeveloperOneDemo <92218513+DeveloperOneDemo@users.noreply.github.com>
2022-03-15 20:28:40 -0400

    dockercompose

--- dbscript.sql
+++ dbscript.sql
@@ -1,14 +1,12 @@
 CREATE DATABASE bms;
 
-\c bms;
-
-CREATE TABLE book_details(book_id INT GENERATED ALWAYS AS IDENTITY, 
-						  book_title VARCHAR(100), 
-						  book_author VARCHAR(20), 
-						  book_genre VARCHAR(20), 
-						  book_cost INT, 
-						  book_image_url VARCHAR(200), 
-						  PRIMARY KEY(book_id));
+CREATE TABLE IF NOT EXISTS bms.book_details(book_id INT GENERATED ALWAYS AS IDENTITY, 
+										  book_title VARCHAR(100), 
+										  book_author VARCHAR(20), 
+										  book_genre VARCHAR(20), 
+										  book_cost INT, 
+										  book_image_url VARCHAR(200), 
+										  PRIMARY KEY(book_id));
 						  
 INSERT INTO book_details(book_title, book_author, book_genre, book_cost, book_image_url) 
 		VALUES('Harry Potter and the Chamber of Secrets', 'J.K.Rowling', 'Fiction', 60, '');
